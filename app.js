adc13f14bc665fc851a4c71c976f50623268d2c9
Nek <matrosov.nikita94@gmail.com>
2016-01-29 18:22:58 +0300

    порт берется из параметров запуска для хостинга

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var conf = require('./config/config');
 var Server = require('./libs/http_server');
-var server = new Server(conf.get("port"),conf.get("host"));
+var port = process.env.PORT || conf.get("port");
+var server = new Server(port, conf.get("host"));
 
 server.run();
