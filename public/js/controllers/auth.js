5d73eddc8f7faa4940650ef8f895b53ef1950fe4
Nek <matrosov.nikita94@gmail.com>
2016-02-04 20:17:27 +0300

    Загрузка категорий изменена

--- public/js/controllers/auth.js
+++ public/js/controllers/auth.js
@@ -9,34 +9,46 @@ $(document).ready(function () {
     var formRegister = $("#signup");
     var formLogin = $("#signin");
     var spinner = Config.spinner;
-    var tree = document.createElement("select");
-    var default_option = document.createElement("option");
-    default_option.setAttribute("selected","selected");
-    default_option.style.display = "none";
-    default_option.text = "Выберите категорию";
-    tree.appendChild(default_option);
+    var tree = document.createElement("ul");
     var foo = function(array,parent)
     {
         for(var i = 0; i < array.length; i++)
         {
             if (array[i].children.length>0)
             {
-                var optgroup = document.createElement("optgroup");
-                optgroup.setAttribute("label",array[i].name);
-                parent.appendChild(optgroup);
-                foo(array[i].children,optgroup);
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.innerHTML = array[i].name;
+                li.appendChild(a);
+                var ul = document.createElement("ul");
+                ul.style.marginLeft = '25px';
+                li.appendChild(ul);
+                parent.appendChild(li);
+                foo(array[i].children,ul);
             }
             else
             {
-                var option = document.createElement("option");
-                option.text = array[i].name;
-                option.value = array[i].id;
-                parent.appendChild(option);
+                var li = document.createElement("li.last");
+                var a = document.createElement("a");
+                a.innerHTML = array[i].name;
+                a.setAttribute("id",array[i].id);
+                li.appendChild(a);
+                parent.appendChild(li);
             }
 
         }
         return parent;
     };
+
+    $("#categories").on('click','li a', function() {
+        var arr = $(this).siblings('ul');
+        if (arr.length==1) {
+            $(arr[0]).slideToggle();
+        } else {
+            $('#categories a').css("background-color","#fff").removeClass("active");
+            $(this).css("background-color","#8FA0FF").addClass("active");
+        }
+    });
    document.getElementById("categories").appendChild(foo(all_categories,tree));
     //регистрация компании
     formRegister.submit(function (e) {
